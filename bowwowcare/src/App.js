84217cdf23b78ad85e57dd4208b77ed410a238cd
suzinxix <suzinxix@gmail.com>
2022-11-26 14:50:22 +0900

    [#4] feat: add home screen

--- bowwowcare/src/App.js
+++ bowwowcare/src/App.js
@@ -1,14 +1,15 @@
 import React from 'react';
+import { Routes, Route, Link } from 'react-router-dom'
+
+import HomePage from './views/HomePage';
 
 import "tailwindcss/tailwind.css";
 
 function App() {
   	return (
-		<div class="container mx-auto w-screen h-screen p-4 flex items-center justify-center">
-			<h1 class="text-2xl font-bold">
-				멍멍케어
-			</h1>
-		</div>
+		<Routes>
+			<Route path="/" element={ <HomePage/> } />
+		</Routes>
   	);
 }
 
