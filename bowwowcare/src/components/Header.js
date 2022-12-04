0d21ec26c22204a61a696450a6ae12b84f7c4dcf
0hee0 <suhee0206@gmail.com>
2022-12-04 17:51:42 +0900

    [#31] feat: update app logo - update app logo ui - add link to /

--- bowwowcare/src/components/Header.js
+++ bowwowcare/src/components/Header.js
@@ -1,11 +1,19 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
+
 
 function Header() {
+  const navigate = useNavigate();
+
+  const handleGoHome = (e) => {
+    navigate("/");
+  }
+
   return (
     <div>
       <div className="flex flex-row my-10">
-        <div className="basis-2/4 text-lg font-bold text-left text-main-color">
-          BowwowCare 🐾
+        <div className="basis-2/4 text-lg font-bold text-left text-main-color font-Tenada" onClick={handleGoHome}>
+          멍멍케어
         </div>
         <div className="basis-1/4"></div>
         <button className="basis-1/4 text-right">로그인</button>
