5d372fdd5a056e29e8106cb49bc65edf8e8f62b9
0hee0 <suhee0206@gmail.com>
2023-02-19 22:05:55 +0900

    [#49] feat: add abnormal behavior selection page

---
+++ bowwowcare/src/views/BehaviorSelectionPage/BehaviorSelectionPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from "../../components/Header";
+
+function BehaviorSelectionPage() {
+    return (
+        <div className="container mx-auto w-screen h-screen px-8">
+            <Header />
+            <div className="mt-12 mb-6">어떤 아이의 이상행동을 확인하는 건가요?</div>
+        </div>
+    )
+}
+
+export default BehaviorSelectionPage
