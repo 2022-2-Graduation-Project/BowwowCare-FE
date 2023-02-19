1994d86f0efc97ae37957e719d180e915898184e
0hee0 <suhee0206@gmail.com>
2023-02-19 22:06:20 +0900

    [#49] feat: implement select box

--- bowwowcare/src/views/BehaviorSelectionPage/BehaviorSelectionPage.js
+++ bowwowcare/src/views/BehaviorSelectionPage/BehaviorSelectionPage.js
@@ -1,11 +1,34 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Header from "../../components/Header";
+import SelectBox from "./Sections/SelectBox";
+import HAPPY from "../../assets/images/happy.png";
 
 function BehaviorSelectionPage() {
+    const [pets, setPets] = useState([]);
+    const [selectedPet, setSelectedPet] = useState({});
+
+    useEffect(() => {
+        // TEST: Mockup Data
+        setPets([
+            {"petId": 1, "petName": "첫 번째 강쥐", "petImage": HAPPY},
+            {"petId": 2, "petName": "두 번째 강쥐", "petImage": HAPPY},
+        ]);
+
+        // TODO: GET pets
+        
+    }, []);
+
+    useEffect(() => {
+        if (pets) {
+            setSelectedPet(pets[0]);
+        }
+    }, [pets]);
+
     return (
         <div className="container mx-auto w-screen h-screen px-8">
             <Header />
             <div className="mt-12 mb-6">어떤 아이의 이상행동을 확인하는 건가요?</div>
+            <SelectBox pets={pets} selectedPet={selectedPet} setSelectedPet={setSelectedPet} />
         </div>
     )
 }
