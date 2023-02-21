e89e30470d43a57c1861e7bc07e076f65e88d83d
suzinxix <suzinxix@gmail.com>
2023-02-21 15:15:48 +0900

    [#4] feat: edit pet list

--- bowwowcare/src/views/HomePage/PetList/PetList.js
+++ bowwowcare/src/views/HomePage/PetList/PetList.js
@@ -4,17 +4,15 @@ import { Link, useNavigate } from "react-router-dom";
 import PetInfo from "./Sections/PetInfo";
 
 function PetList(props) {
-  const token = window.localStorage.getItem("user");
+  const user = window.localStorage.getItem("user");
   const navigate = useNavigate();
-  return (
+  return user ? (
     <div className="flex overflow-x-scroll">
       {props.pets.map((pet, i) => {
-        return token ? (
+        return (
           <Link to={`/petinfo/${pet.id}`} state={{ pet: pet }}>
             <PetInfo pet={pet} key={i} />
           </Link>
-        ) : (
-          ""
         );
       })}
       <div>
@@ -25,6 +23,15 @@ function PetList(props) {
           <p>+</p>가족을 추가해주세요
         </button>
       </div>
+    </div>
+  ) : (
+    <div>
+      <button
+        className="w-80 h-28 shadow m-1 rounded-lg border-gray-300 hover:border-main-color text-gray-300 bg-transparent pl-4"
+        onClick={() => navigate("/addition")}
+      >
+        <p>+</p>가족을 추가해주세요
+      </button>
     </div>
   );
 }
