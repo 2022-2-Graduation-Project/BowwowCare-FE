6f347dc4bf54e6c7b1e56259af193b10e202f219
suzinxix <suzinxix@gmail.com>
2023-02-27 20:38:22 +0900

    [#4] feat: update home screen ui

--- bowwowcare/src/views/HomePage/HomePage.js
+++ bowwowcare/src/views/HomePage/HomePage.js
@@ -62,18 +62,34 @@ function HomePage() {
     <div className="container mx-auto px-8 w-screen h-screen">
       <Header />
       <div className="flex">{petList?.length && <PetList pets={petList} />}</div>
-      
-      <p className="text-xl font-bold mt-16">우리 아이 이상행동 확인해보기</p>
+
+      <div className="mt-14 py-8 shadow-lg rounded-lg">
+
+        <div className="text-center">
+        <p className=" text-lg">우리 아이가 이상행동을 보이나요?</p>
+        <p className=" text-xs">간단한 진단을 통해 솔루션을 확인해보세요</p>
+        </div>
+
       <div className="flex justify-center mt-6 text-gray-500 text-center text-sm">
+        <button onClick={() => handleNavigation("aggression")} className=" border-r">
+          <img src={aggression}></img>
+          <p>공격적인 아이</p>
+        </button>
+
         <button onClick={() => handleNavigation("anxiety")}>
           <img src={anxiety}></img>
-          <p>불안에 떨고 있어요</p>
-        </button>
-        <button onClick={() => handleNavigation("aggression")}>
-          <img src={aggression}></img>
-          <p>으르렁 거리고 있어요</p>
+          <p>불안해보이는 아이</p>
         </button>
       </div>
+      </div>
+
+      <div className="mt-8 p-8 shadow-lg rounded-lg">
+        <button>
+          <p className=" text-lg">우리 아이의 감정을 확인해보세요</p>
+          <p className=" text-xs text-left">사진 한 장으로 간편하게 알아보세요</p>
+        </button>
+      </div>
+      
 
       <p className="text-xl font-bold mt-16">
         우리 아이 표정으로 감정 알아보기
