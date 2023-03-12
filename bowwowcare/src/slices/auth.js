9894dd86686061d7458fdc1baed460ff7dd20c17
hee-suh <suhee0206@gmail.com>
2023-03-12 16:09:39 +0900

    [#37] fix: fix signup error - error1: cannot read properties of undefined (reading 'message') - error2: not navigate to /login when sign up success

--- bowwowcare/src/slices/auth.js
+++ bowwowcare/src/slices/auth.js
@@ -6,46 +6,35 @@ import AuthService from "../services/auth.service";
 const user = JSON.parse(localStorage.getItem("user"));
 
 export const signup = createAsyncThunk(
-    "auth/signup",
-    async (data, thunkAPI) => {
-        try {
-            const response = await AuthService.signup(data);
-            thunkAPI.dispatch(setMessage(response.data.message));
-            return response.data;
-        } catch (error) {
-            const message =
-                (error.response &&
-                error.response.data &&
-                error.response.data.message) ||
-                error.message ||
-                error.toString();
-            thunkAPI.dispatch(setMessage(message));
-            return thunkAPI.rejectWithValue();
-        }
+  "auth/signup",
+  async (data, thunkAPI) => {
+    try {
+      const response = await AuthService.signup(data);
+      thunkAPI.dispatch(setMessage(response.data.message));
+      return response.data;
+    } catch (error) {
+      const message =
+        error.response && error.response.data && error.response.data.message;
+      thunkAPI.dispatch(setMessage(message));
+      return thunkAPI.rejectWithValue();
     }
+  }
 );
 
-export const login = createAsyncThunk(
-    "auth/login",
-    async (data, thunkAPI) => {
-        try {
-            const userData = await AuthService.login(data);
-            return { user: userData };
-        } catch (error) {
-            const message =
-                (error.response &&
-                error.response.data &&
-                error.response.data.message) ||
-                error.message ||
-                error.toString();
-            thunkAPI.dispatch(setMessage(message));
-            return thunkAPI.rejectWithValue();
-        }
-    }
-);
+export const login = createAsyncThunk("auth/login", async (data, thunkAPI) => {
+  try {
+    const userData = await AuthService.login(data);
+    return { user: userData };
+  } catch (error) {
+    const message =
+      error.response && error.response.data && error.response.data.message;
+    thunkAPI.dispatch(setMessage(message));
+    return thunkAPI.rejectWithValue();
+  }
+});
 
 export const logout = createAsyncThunk("auth/logout", async () => {
-    await AuthService.logout();
+  await AuthService.logout();
 });
 
 const initialState = user
@@ -53,28 +42,28 @@ const initialState = user
   : { isLoggedIn: false, user: null };
 
 const authSlice = createSlice({
-    name: "auth",
-    initialState,
-    extraReducers: {
-        [signup.fulfilled]: (state, action) => {
-        state.isLoggedIn = false;
-        },
-        [signup.rejected]: (state, action) => {
-        state.isLoggedIn = false;
-        },
-        [login.fulfilled]: (state, action) => {
-        state.isLoggedIn = true;
-        state.user = action.payload.user;
-        },
-        [login.rejected]: (state, action) => {
-        state.isLoggedIn = false;
-        state.user = null;
-        },
-        [logout.fulfilled]: (state, action) => {
-        state.isLoggedIn = false;
-        state.user = null;
-        },
+  name: "auth",
+  initialState,
+  extraReducers: {
+    [signup.fulfilled]: (state, action) => {
+      state.isLoggedIn = false;
     },
+    [signup.rejected]: (state, action) => {
+      state.isLoggedIn = false;
+    },
+    [login.fulfilled]: (state, action) => {
+      state.isLoggedIn = true;
+      state.user = action.payload.user;
+    },
+    [login.rejected]: (state, action) => {
+      state.isLoggedIn = false;
+      state.user = null;
+    },
+    [logout.fulfilled]: (state, action) => {
+      state.isLoggedIn = false;
+      state.user = null;
+    },
+  },
 });
 
 const { reducer } = authSlice;
