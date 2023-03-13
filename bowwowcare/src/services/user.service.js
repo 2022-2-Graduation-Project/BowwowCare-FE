c88947eb8deee66c0ae77d5e1c7a0af5847a36e3
suzinxix <suzinxix@gmail.com>
2023-03-14 04:54:53 +0900

    [#86] feat: update user service api url

--- bowwowcare/src/services/user.service.js
+++ bowwowcare/src/services/user.service.js
@@ -1,15 +1,13 @@
 import axios from "axios";
 import authHeader from "./auth-header";
-//import { API_URL } from "../Config";
-
-const API_URL = "http://localhost:8080/api/user";
+import { API_URL } from "../Config";
 
 const getPublicContent = () => {
   return axios.get(API_URL + "all");
 };
 
 const getUserBoard = () => {
-  return axios.get("http://localhost:8080/api/v1/user" , { headers: authHeader() });
+  return axios.get(API_URL + "/user" , { headers: authHeader() });
 };
 
 const userService = {
