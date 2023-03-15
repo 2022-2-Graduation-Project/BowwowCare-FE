930b6209c72b33260b858ada4685fbd4e2ac269e
suzinxix <suzinxix@gmail.com>
2023-03-15 13:17:46 +0900

    [#31] feat: insert profile img in header

--- bowwowcare/src/components/Header.js
+++ bowwowcare/src/components/Header.js
@@ -44,7 +44,7 @@ function Header() {
 				null
 			) : (
 				JSON.parse(localStorage.getItem("user")) ? (
-					window.location.pathname === "/user" ? <button className="text-right" onClick={handleLogout}>로그아웃</button> : window.location.pathname === "/"? 
+					window.location.pathname === "/user" ? <button className="text-right" onClick={handleLogout}>로그아웃</button> : 
 					<button onClick={handleUser}>
 						{isProfile() ? 
 						<img src={isProfile()} className="w-8 h-8 rounded-full"></img>:
@@ -52,7 +52,7 @@ function Header() {
 						<path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6Zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0Zm4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4Zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10Z"/>
 				  		</svg>
 				  }
-					</button> : null
+					</button> 
 				) : (
 					<button className="text-right" onClick={handleLogin}>로그인</button>
 				)
