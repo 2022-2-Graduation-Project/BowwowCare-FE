809d9f222cec69c35ddbe1290b1e2ea6796ec7fc
hee-suh <suhee0206@gmail.com>
2023-03-20 00:59:01 +0900

    [#49] feat: remove mockup data and test data & add response data

--- bowwowcare/src/components/SelectPet.js
+++ bowwowcare/src/components/SelectPet.js
@@ -1,65 +1,128 @@
-import React, { useState, useContext } from 'react';
-import HAPPY from "../assets/images/happy.png";
+import React, { useState, useContext } from "react";
 
-import { ThemeContext } from './../context/ThemeProvider.js';
-import { colorVariants } from '../utils/Dictionary';
+import { ThemeContext } from "./../context/ThemeProvider.js";
+import { colorVariants } from "../utils/Dictionary";
 
 function SelectPet({ pets, selectedPet, setSelectedPet, abnormal }) {
-    const [show, setShow] = useState(false);
-    const [themeMode, setThemeMode] = useContext(ThemeContext);
+  const [show, setShow] = useState(false);
+  const [themeMode, setThemeMode] = useContext(ThemeContext);
 
-    const showSelectMenu = (e) => {
-        if (!abnormal) {
-            setShow(!show);
-        }
+  const showSelectMenu = (e) => {
+    if (!abnormal) {
+      setShow(!show);
     }
+  };
 
-    const handleSelected = (pet) => (e) => {
-        setSelectedPet(pet);
-        showSelectMenu();
-    }
+  const handleSelected = (pet) => (e) => {
+    setSelectedPet(pet);
+    showSelectMenu();
+  };
 
-    return (
-        <div>
-            <div class="relative mt-1">
-                {selectedPet ? (
-                    <button onClick={showSelectMenu} type="button" class={`relative w-full cursor-default rounded-md border border-gray-300 bg-white py-2 pl-3 pr-10 text-left shadow-sm  focus:${colorVariants['border'+themeMode]} ${colorVariants['ring'+themeMode]} focus:outline-none focus:ring-1`} aria-haspopup="listbox" aria-expanded="true" aria-labelledby="listbox-label">
-                        <span class="flex items-center">
-                            <img src={HAPPY} alt="" class="h-6 w-6 flex-shrink-0 rounded-full" />
-                            <span class="ml-3 block truncate">{selectedPet.petName}</span>
-                        </span>
-                        <span class="pointer-events-none absolute inset-y-0 right-0 ml-3 flex items-center pr-2">
-                            <svg class="h-5 w-5 text-gray-400" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-                            <path fill-rule="evenodd" d="M10 3a.75.75 0 01.55.24l3.25 3.5a.75.75 0 11-1.1 1.02L10 4.852 7.3 7.76a.75.75 0 01-1.1-1.02l3.25-3.5A.75.75 0 0110 3zm-3.76 9.2a.75.75 0 011.06.04l2.7 2.908 2.7-2.908a.75.75 0 111.1 1.02l-3.25 3.5a.75.75 0 01-1.1 0l-3.25-3.5a.75.75 0 01.04-1.06z" clip-rule="evenodd" />
-                            </svg>
-                        </span>
-                    </button>
-                ) : null}
-                {pets && show ? (
-                    <ul class="absolute z-10 mt-1 max-h-56 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm" tabindex="-1" role="listbox" aria-labelledby="listbox-label" aria-activedescendant="listbox-option-3">
-                        {pets?.map(pet => 
-                            <li 
-                                key={pet?.petId} 
-                                class={`${pet?.petId === selectedPet?.petId ? colorVariants['bg'+themeMode] + ` text-white` : "text-gray-900"} relative cursor-default select-none py-2 pl-3 pr-9`} 
-                                id="listbox-option-0" role="option"
-                                onClick={handleSelected(pet)}
-                            >
-                                <div class="flex items-center">
-                                    <img src={HAPPY} alt="" class="h-6 w-6 flex-shrink-0 rounded-full" />
-                                    <span class={`${pet?.petId === selectedPet?.petId ? "font-semibold": "font-normal"} ml-3 block truncate`}>{pet?.petName}</span>
-                                </div>
-                                <span class={`${pet?.petId === selectedPet?.petId ? "text-white" : "text-main-color"} ${pet?.petId === selectedPet?.petId ? "absolute" : "hidden"} inset-y-0 right-0 flex items-center pr-4`}>
-                                <svg class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-                                    <path fill-rule="evenodd" d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z" clip-rule="evenodd" />
-                                </svg>
-                                </span>
-                            </li>
-                        )}
-                    </ul>
-                ) : null}
-            </div>
-        </div>
-    )
+  return (
+    <div>
+      <div class="relative mt-1">
+        {selectedPet ? (
+          <button
+            onClick={showSelectMenu}
+            type="button"
+            class={`relative w-full cursor-default rounded-md border border-gray-300 bg-white py-2 pl-3 pr-10 text-left shadow-sm  focus:${
+              colorVariants["border" + themeMode]
+            } ${
+              colorVariants["ring" + themeMode]
+            } focus:outline-none focus:ring-1`}
+            aria-haspopup="listbox"
+            aria-expanded="true"
+            aria-labelledby="listbox-label"
+          >
+            <span class="flex items-center">
+              <img
+                src={selectedPet.petImage}
+                alt=""
+                class="h-6 w-6 flex-shrink-0 rounded-full"
+              />
+              <span class="ml-3 block truncate">{selectedPet.petName}</span>
+            </span>
+            <span class="pointer-events-none absolute inset-y-0 right-0 ml-3 flex items-center pr-2">
+              <svg
+                class="h-5 w-5 text-gray-400"
+                viewBox="0 0 20 20"
+                fill="currentColor"
+                aria-hidden="true"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M10 3a.75.75 0 01.55.24l3.25 3.5a.75.75 0 11-1.1 1.02L10 4.852 7.3 7.76a.75.75 0 01-1.1-1.02l3.25-3.5A.75.75 0 0110 3zm-3.76 9.2a.75.75 0 011.06.04l2.7 2.908 2.7-2.908a.75.75 0 111.1 1.02l-3.25 3.5a.75.75 0 01-1.1 0l-3.25-3.5a.75.75 0 01.04-1.06z"
+                  clip-rule="evenodd"
+                />
+              </svg>
+            </span>
+          </button>
+        ) : null}
+        {pets && show ? (
+          <ul
+            class="absolute z-10 mt-1 max-h-56 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm"
+            tabindex="-1"
+            role="listbox"
+            aria-labelledby="listbox-label"
+            aria-activedescendant="listbox-option-3"
+          >
+            {pets?.map((pet) => (
+              <li
+                key={pet?.petId}
+                class={`${
+                  pet?.petId === selectedPet?.petId
+                    ? colorVariants["bg" + themeMode] + ` text-white`
+                    : "text-gray-900"
+                } relative cursor-default select-none py-2 pl-3 pr-9`}
+                id="listbox-option-0"
+                role="option"
+                onClick={handleSelected(pet)}
+              >
+                <div class="flex items-center">
+                  <img
+                    src={selectedPet.petImage}
+                    alt=""
+                    class="h-6 w-6 flex-shrink-0 rounded-full"
+                  />
+                  <span
+                    class={`${
+                      pet?.petId === selectedPet?.petId
+                        ? "font-semibold"
+                        : "font-normal"
+                    } ml-3 block truncate`}
+                  >
+                    {pet?.petName}
+                  </span>
+                </div>
+                <span
+                  class={`${
+                    pet?.petId === selectedPet?.petId
+                      ? "text-white"
+                      : "text-main-color"
+                  } ${
+                    pet?.petId === selectedPet?.petId ? "absolute" : "hidden"
+                  } inset-y-0 right-0 flex items-center pr-4`}
+                >
+                  <svg
+                    class="h-5 w-5"
+                    viewBox="0 0 20 20"
+                    fill="currentColor"
+                    aria-hidden="true"
+                  >
+                    <path
+                      fill-rule="evenodd"
+                      d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z"
+                      clip-rule="evenodd"
+                    />
+                  </svg>
+                </span>
+              </li>
+            ))}
+          </ul>
+        ) : null}
+      </div>
+    </div>
+  );
 }
 
-export default SelectPet
+export default SelectPet;
--- bowwowcare/src/views/SelectionPage/SelectionPage.js
+++ bowwowcare/src/views/SelectionPage/SelectionPage.js
@@ -1,6 +1,6 @@
-import React, { useEffect, useState, useContext } from 'react';
-import { useNavigate, useLocation } from 'react-router-dom';
-import axios from 'axios';
+import React, { useEffect, useState, useContext } from "react";
+import { useNavigate, useLocation } from "react-router-dom";
+import axios from "axios";
 import { API_URL } from "../../Config";
 import authHeader from "../../services/auth-header";
 import Header from "../../components/Header";
@@ -8,141 +8,163 @@ import SelectPet from "../../components/SelectPet";
 import ButtonGroups from "./Sections/ButtonGroups";
 import HAPPY from "../../assets/images/happy.png";
 import Button from "../../components/Button";
-import { ThemeContext } from '../../context/ThemeProvider';
+import { ThemeContext } from "../../context/ThemeProvider";
 
+function SelectionPage() {
+  const navigate = useNavigate();
+  const location = useLocation();
+  const [pets, setPets] = useState([]);
+  const [selectedPet, setSelectedPet] = useState({});
+  const [behaviors, setBehaviors] = useState([]);
+  const [selectedBehaviors, setSelectedBehaviors] = useState([]);
+  const [abnormal, setAbnormal] = useState(false);
+  const [aggressionType, setAggressionType] = useState([]);
+  const [themeMode, setThemeMode] = useContext(ThemeContext);
 
-function SelectionPage() {  
-	const navigate = useNavigate();
-    const location = useLocation();
-    const [pets, setPets] = useState([]);
-    const [selectedPet, setSelectedPet] = useState({});
-    const [behaviors, setBehaviors] = useState([]);
-    const [selectedBehaviors, setSelectedBehaviors] = useState([]);
-    const [abnormal, setAbnormal] = useState(false);
-    const [aggressionType, setAggressionType] = useState([]);
-    const [themeMode, setThemeMode] = useContext(ThemeContext);
+  useEffect(() => {
+    // TEST: Mockup Data
+    // setPets([
+    //     {"petId": 1, "petName": "첫 번째 강쥐", "petImage": HAPPY},
+    //     {"petId": 2, "petName": "두 번째 강쥐", "petImage": HAPPY},
+    // ]);
+    setBehaviors([
+      { aggressionId: 1, aggressionType: 0, content: "헐떡이며 숨쉬기" },
+      { aggressionId: 2, aggressionType: 0, content: "동공확장" },
+      { aggressionId: 3, aggressionType: 0, content: "몸 떨기" },
+      { aggressionId: 4, aggressionType: 0, content: "털 세우기" },
+      { aggressionId: 5, aggressionType: 1, content: "입 다물기" },
+      { aggressionId: 6, aggressionType: 1, content: "경직되기" },
+      { aggressionId: 7, aggressionType: 1, content: "짖기" },
+      { aggressionId: 8, aggressionType: 1, content: "응시하기" },
+      { aggressionId: 9, aggressionType: 2, content: "이빨 보이기" },
+      { aggressionId: 10, aggressionType: 2, content: "도망/철수" },
+      { aggressionId: 11, aggressionType: 2, content: "달려들기 & 한 번 물기" },
+    ]);
 
-    useEffect(() => {
-        // TEST: Mockup Data
-        setPets([
-            {"petId": 1, "petName": "첫 번째 강쥐", "petImage": HAPPY},
-            {"petId": 2, "petName": "두 번째 강쥐", "petImage": HAPPY},
-        ]);
-        setBehaviors([
-            {"aggressionId": 1, "aggressionType": 0, "content": "헐떡이며 숨쉬기"},
-            {"aggressionId": 2, "aggressionType": 0, "content": "동공확장"},
-            {"aggressionId": 3, "aggressionType": 0, "content": "몸 떨기"},
-            {"aggressionId": 4, "aggressionType": 0, "content": "털 세우기"},
-            {"aggressionId": 5, "aggressionType": 1, "content": "입 다물기"},
-            {"aggressionId": 6, "aggressionType": 1, "content": "경직되기"},
-            {"aggressionId": 7, "aggressionType": 1, "content": "짖기"},
-            {"aggressionId": 8, "aggressionType": 1, "content": "응시하기"},
-            {"aggressionId": 9, "aggressionType": 2, "content": "이빨 보이기"},
-            {"aggressionId": 10, "aggressionType": 2, "content": "도망/철수"},
-            {"aggressionId": 11, "aggressionType": 2, "content": "달려들기 & 한 번 물기"},
-        ]);
-
-        axios({
-            method: 'get',
-            url: `${API_URL}/pets`,
-            headers: authHeader()
-        })
-        .then(response => {
-            if (response.status === 200) {
-                if (response.data?.length) {
-                    setPets(response.data);
-                }
-            }
-        })
-        .catch(error => {
-            console.log(error?.response);
-        });
-
-        axios({
-            method: 'get',
-            url: `${API_URL}/behaviors`,
-        })
-        .then(response => {
-            if (response.status === 200) {
-                setBehaviors(response.data);
-            }
-        })
-        .catch(error => {
-            console.log(error?.response);
-        });
-    }, []);
-
-    useEffect(() => {
-        if (pets) {
-            setSelectedPet(pets[0]);
+    axios({
+      method: "get",
+      url: `${API_URL}/pets`,
+      headers: authHeader(),
+    })
+      .then((response) => {
+        if (response.status === 200) {
+          if (response.data?.length) {
+            setPets(response.data);
+          }
         }
-    }, [pets]);
+      })
+      .catch((error) => {
+        console.log(error?.response);
+      });
 
-    useEffect(() => {
-        if (behaviors) {
-            setSelectedBehaviors(behaviors.map(item => ({ ...item, isDeleted: true })));
+    axios({
+      method: "get",
+      url: `${API_URL}/behaviors`,
+    })
+      .then((response) => {
+        if (response.status === 200) {
+          setBehaviors(response.data);
         }
-    }, [behaviors]);
+      })
+      .catch((error) => {
+        console.log(error?.response);
+      });
+  }, []);
 
-    const showBehaviors = (e) => {
-        if (selectedBehaviors.some(item => item.isDeleted===false)) {
-            setAbnormal(true);
-            let selected = selectedBehaviors
-                            .filter(item => item.isDeleted===false)
-                            .map(item => item.aggressionType);
-            let unique = [...new Set(selected)];
-            setAggressionType(unique);
-        }
+  useEffect(() => {
+    if (pets?.length) {
+      setSelectedPet(pets[0]);
     }
+  }, [pets]);
 
-    const handleExamination = () => {
-        let state = {
-            "type": location.state.type,
-            "petId": selectedPet?.petId
-        }
-        if (location?.state?.type==="aggression") {
-            state.aggressionType = aggressionType
-        }
-        navigate("/examination", {
-			state: state
-		})   
+  useEffect(() => {
+    if (behaviors) {
+      setSelectedBehaviors(
+        behaviors.map((item) => ({ ...item, isDeleted: true }))
+      );
     }
-    
+  }, [behaviors]);
 
-    return (
-        <div className="container mx-auto w-screen h-screen px-8">
-            <Header />
-            {location?.state?.type ? (
-                <div className="h-5/6 flex flex-col justify-between">
-                    <div className="h-2/3">
-                        <div className="mt-4 mb-2">어떤 아이의 이상행동을 확인하는 건가요?</div>
-                        <SelectPet abnormal={abnormal} pets={pets} selectedPet={selectedPet} setSelectedPet={setSelectedPet} />
-                        {location.state.type==="aggression" ? (
-                            <div>
-                                <div className="mt-4 mb-2">아이가 어떤 <span className="font-bold">행동</span>을 보이고 있나요?</div>
-                                <ButtonGroups abnormal={abnormal} behaviors={behaviors} selectedBehaviors={selectedBehaviors} setSelectedBehaviors={setSelectedBehaviors} />
-                            </div>
-                        ) : null}
-                    </div>
-                    {location.state.type==="aggression" ? (
-                        <div className="w-full">
-                            {abnormal ? (
-                                <div className="text-center mb-4">
-                                    <span>문진을 통해 공격 행동 교정을 위한 <br/></span>
-                                    <span>솔루션을 얻어볼까요?</span>
-                                </div>
-                            ) : null}
-                            <Button onClick={!abnormal ? showBehaviors : handleExamination} bgColor={themeMode}>다음</Button>
-                        </div>
-                    ) : (
-                        location.state.type==="anxiety" ? (
-                            <Button onClick={handleExamination} bgColor={themeMode}>다음</Button>
-                        ) : null
-                    )}
+  const showBehaviors = (e) => {
+    if (selectedBehaviors.some((item) => item.isDeleted === false)) {
+      setAbnormal(true);
+      let selected = selectedBehaviors
+        .filter((item) => item.isDeleted === false)
+        .map((item) => item.aggressionType);
+      let unique = [...new Set(selected)];
+      setAggressionType(unique);
+    }
+  };
+
+  const handleExamination = () => {
+    let state = {
+      type: location.state.type,
+      petId: selectedPet?.petId,
+    };
+    if (location?.state?.type === "aggression") {
+      state.aggressionType = aggressionType;
+    }
+    navigate("/examination", {
+      state: state,
+    });
+  };
+
+  return (
+    <div className="container mx-auto w-screen h-screen px-8">
+      <Header />
+      {location?.state?.type ? (
+        <div className="h-5/6 flex flex-col justify-between">
+          <div className="h-2/3">
+            <div className="mt-4 mb-2">
+              어떤 아이의 이상행동을 확인하는 건가요?
+            </div>
+            <SelectPet
+              abnormal={abnormal}
+              pets={pets}
+              selectedPet={selectedPet}
+              setSelectedPet={setSelectedPet}
+            />
+            {location.state.type === "aggression" ? (
+              <div>
+                <div className="mt-4 mb-2">
+                  아이가 어떤 <span className="font-bold">행동</span>을 보이고
+                  있나요?
                 </div>
+                <ButtonGroups
+                  abnormal={abnormal}
+                  behaviors={behaviors}
+                  selectedBehaviors={selectedBehaviors}
+                  setSelectedBehaviors={setSelectedBehaviors}
+                />
+              </div>
             ) : null}
+          </div>
+          {location.state.type === "aggression" ? (
+            <div className="w-full">
+              {abnormal ? (
+                <div className="text-center mb-4">
+                  <span>
+                    문진을 통해 공격 행동 교정을 위한 <br />
+                  </span>
+                  <span>솔루션을 얻어볼까요?</span>
+                </div>
+              ) : null}
+              <Button
+                onClick={!abnormal ? showBehaviors : handleExamination}
+                bgColor={themeMode}
+              >
+                다음
+              </Button>
+            </div>
+          ) : location.state.type === "anxiety" ? (
+            <Button onClick={handleExamination} bgColor={themeMode}>
+              다음
+            </Button>
+          ) : null}
         </div>
-    )
+      ) : null}
+    </div>
+  );
 }
 
-export default SelectionPage
+export default SelectionPage;
